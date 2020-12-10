812ea5435c5c494f23d5eecf9f259a7d2ea4d5fb
João Pedro Silva de Queiroz <joaopedrosilvaqueiroz@gmail.com>
2020-12-10 04:17:05 -0300

    História do Dub WIP

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,36 @@
+// Slides Script
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    let descs = document.getElementsByClassName("artist-desc");
+
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        descs[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    descs[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Função pra mostrar o menu mobile
 function showMenu() {
     let menu = document.getElementById("menu-hamburguer");
 
